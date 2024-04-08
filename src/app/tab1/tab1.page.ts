f423a0116eb6f1b8a6bfb41b2b0f70457830ed4a
DevDaniloFerrari <daniloferrari2000@hotmail.com>
2024-04-08 18:36:57 -0300

    add new task

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -3,12 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
-
   constructor() {}
 
-  tasks = ['task 1', 'task 2', 'task 3']
+  newTask: string = '';
+  tasks: string[] = ['task 1', 'task 2', 'task 3'];
 
+  addTask() {
+    this.tasks.push(this.newTask);
+  }
 }
